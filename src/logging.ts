aa64f84345362e43110a2055dda4882f4d96e5b1
Glitch (hmdwatest) <none>
2019-12-23 06:09:21 +0000

    json file resolution working well with packpath. log4js.json refresh causing errors (not blocking)

--- src/logging.ts
+++ src/logging.ts
@@ -1,11 +1,18 @@
-let app_root_dir = require('app-root-dir').get();
+
+import { sleep } from 'howsmydriving-utils';
 
 import { __MODULE_NAME__ } from './server';
 
 const chokidar = require('chokidar'),
-  log4js = require('log4js'),
   path = require('path'),
-  config_path = path.resolve(app_root_dir + '/dist/config/log4js.json');
+  packpath = require('packpath');
+
+let log4js = require('log4js');
+
+let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
+let packpath_self = packpath.self();
+
+const config_path = path.resolve(packpath_parent + '/dist/config/log4js.json');
 
 // Load the config.
 log4js.configure(config_path);
@@ -17,7 +24,7 @@ export let log = log4js.getLogger('result'),
 log.addContext('module', __MODULE_NAME__);
 lastdmLog.addContext('module', __MODULE_NAME__);
 lastmentionLog.addContext('module', __MODULE_NAME__);
-log.info(`app_root_dir: ${app_root_dir}.`);
+log.info(`log4js.json: ${config_path}.`);
 
 
 /**
@@ -41,6 +48,29 @@ var watcher = chokidar.watch(config_path, {
  **/
 function reloadlog(reason: string) {
   log.info(`Reloading log config due to config file ${reason}.`);
+
+  // Leave the current log active until after we replace it.
+  // This should allow any code with a stale instance to keep logging
+  // until they are done.
+  try {
+    log4js.shutdown( (err: Error) => {
+      if (err) {
+        log.error(`Error occurred during log shutdown: ${err}.`);
+      }
+    });
+  } catch (err) {
+      log.error(`Error occurred during log shutdown: ${err}.`);
+  }
+
+  log4js.configure(config_path);
+  log = log4js.getLogger('reason');
+  lastdmLog = log4js.getLogger('_lastdm');
+  lastmentionLog = log4js.getLogger('_lastdm');
+  log.addContext('module', __MODULE_NAME__);
+  lastdmLog.addContext('module', __MODULE_NAME__);
+  lastmentionLog.addContext('module', __MODULE_NAME__);
+
+  /*
   log4js.shutdown(() => {
     log4js.configure(config_path);
     log = log4js.getLogger('reason');
@@ -51,13 +81,18 @@ function reloadlog(reason: string) {
     lastdmLog.addContext('module', __MODULE_NAME__);
     lastmentionLog.addContext('module', __MODULE_NAME__);
   });
+  */
 }
 
 // Handle the change/add events for the log4js config file.
 watcher
   .on('add', (path: string) => {
-    reloadlog(`add of ${path}`);
+    sleep(2000).then( () => {
+      reloadlog(`add of ${path}`);
+    })
   })
   .on('change', (path: string) => {
-    reloadlog(`change of ${path}`);
+    sleep(2000).then( () => {
+      reloadlog(`change of ${path}`);
+    });
   });
